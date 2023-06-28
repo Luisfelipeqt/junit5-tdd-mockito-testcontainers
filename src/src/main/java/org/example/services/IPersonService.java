29b1cbaea5ea2805f9a6687ac4acd7e5e47fd225
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-06-27 21:12:13 -0300

    Aplicando TDD

---
+++ src/src/main/java/org/example/services/IPersonService.java
@@ -0,0 +1,7 @@
+package org.example.services;
+
+import domain.Person;
+
+public interface IPersonService {
+    Person createPerson(Person person);
+}
---
+++ src/src/main/java/org/example/services/PersonService.java
@@ -0,0 +1,11 @@
+package org.example.services;
+
+import domain.Person;
+
+public class PersonService implements IPersonService{
+    @Override
+    public Person createPerson(Person person) {
+
+        return person;
+    }
+}
